c81d796a35f6731141f04532172d15d8ca7e519b
davischristianson <dav.christian10@gmail.com>
2023-08-01 20:23:26 -0700

    Added greeting

--- BranchingPractice/Form1.Designer.cs
+++ BranchingPractice/Form1.Designer.cs
@@ -28,18 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            btnDisplayGreeting = new Button();
+            txtName = new TextBox();
             SuspendLayout();
+            // 
+            // btnDisplayGreeting
+            // 
+            btnDisplayGreeting.Location = new Point(105, 120);
+            btnDisplayGreeting.Name = "btnDisplayGreeting";
+            btnDisplayGreeting.Size = new Size(100, 45);
+            btnDisplayGreeting.TabIndex = 0;
+            btnDisplayGreeting.Text = "Display Greeting";
+            btnDisplayGreeting.UseVisualStyleBackColor = true;
+            btnDisplayGreeting.Click += btnDisplayGreeting_Click;
+            // 
+            // txtName
+            // 
+            txtName.Location = new Point(105, 91);
+            txtName.Name = "txtName";
+            txtName.Size = new Size(100, 23);
+            txtName.TabIndex = 1;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(txtName);
+            Controls.Add(btnDisplayGreeting);
             Name = "Form1";
             Text = "Branching Practice";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Button btnDisplayGreeting;
+        private TextBox txtName;
     }
 }
